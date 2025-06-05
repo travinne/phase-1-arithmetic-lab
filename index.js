4e1102c6cd0969c9962b902d534e056c73e3612b
travinne <travinne.mutahi@moringaschool.com>
2025-06-05 12:05:26 +0300

    arthmetics

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() > 0)
+
+let num3 = 16
+let num4 = 6
+
+let mod = num3 % num4
+
+let max = Math.max(1,4,6,7,8,10,20)
